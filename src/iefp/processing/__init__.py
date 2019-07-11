2a78078a7cb0a46aec11b7848e8e94573fcbf485
Rosa Lavelle-Hill <51444424+Rosa-Lavelle-Hill@users.noreply.github.com>
2019-07-11 12:29:45 +0100

    Clean (#59)
    * Add notebooks with 2 functions for cleaning:
    
    cleaning of strings and dates
    
    * Add clean functions in source code
    
    * Add notebook which tests clean functions
    
    * removed and added white lines
    
    * Changed string clean function, now it works
    
    * Add working source files for cleaning functions and updated notebooks

--- src/iefp/processing/__init__.py
+++ src/iefp/processing/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
 
-from .cleaning import bool_convert, strip_time
+from .cleaning import clean_string, object_to_date, bool_convert, strip_time
--- src/iefp/processing/cleaning.py
+++ src/iefp/processing/cleaning.py
@@ -1,6 +1,24 @@
 import pandas as pd
 
 
+def clean_string(column):
+    "function to make string column lower case and remove characters defined in list"
+    column = column.str.lower()
+    characters = ['(', ')', '-', '+', '  ']
+    for char in characters:
+        column = column.str.replace(char, "", case=False, regex=False)
+
+    return column
+
+
+def object_to_date(column, inputformat):
+    """converts object column to string, then to datetime, then to date"""
+    column = column.astype("int64").astype("str")
+    column = pd.to_datetime(column, errors='coerce', format=inputformat)
+    column = column.dt.date
+    return column
+
+
 def bool_convert(dataframe, bool_list):
     bool_dict = {"S": 1, "N": 0}
     for col in bool_list:
