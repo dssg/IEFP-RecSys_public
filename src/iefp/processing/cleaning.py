4b6c1c8ba185eedf0c294f973f36a191eb47d3a8
Nathan Cruz Coulson <nathancoulson@icloud.com>
2019-07-13 10:31:27 +0100

    Make string transformation function more efficient. Add docstrings to other cleaning functions (#65)

--- src/iefp/processing/cleaning.py
+++ src/iefp/processing/cleaning.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 
-def clean_string(column):
-    "function to make string column lower case and remove characters defined in list"
-    column = column.str.lower()
+def clean_string(df):
+    """function to make string column lower case and remove characters defined in list"""
+    df = df.applymap(lambda s:s.lower() if type(s) == str else s)
     characters = ['(', ')', '-', '+', '  ', '.']
     for char in characters:
-        column = column.str.replace(char, "", case=False, regex=False)
-    return column
+        df = df.apply(lambda s: s.replace(char, "", regex=False))
+    return df
 
 
 def object_to_date(column, inputformat):
@@ -19,6 +19,7 @@ def object_to_date(column, inputformat):
 
 
 def bool_convert(dataframe, bool_list):
+    """maps yes/no fields to 1/0 and then converts to boolean"""
     bool_dict = {"S": 1, "N": 0}
     for col in bool_list:
         dataframe[col] = dataframe[col].map(bool_dict)
@@ -27,6 +28,7 @@ def bool_convert(dataframe, bool_list):
 
 
 def strip_time(dataframe, date_list):
+    """strips time element from datetime columns"""
     for date_col in date_list:
         dataframe[date_col] = pd.to_datetime(dataframe[date_col].dt.date)
     return dataframe
