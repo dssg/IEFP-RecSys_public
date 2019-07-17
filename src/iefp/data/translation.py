cbdc0f398add1f86e596e318e8635cc6366f25f1
Tobias Richter <tr4711@gmail.com>
2019-07-17 15:55:32 +0100

    Delete unused files (#79)

--- src/iefp/data/translation.py
+++
@@ -1,6 +0,0 @@
-def translate_columns(tables, trans_dict):
-    '''Function to translate column names for a list of tables'''
-    for df_table in tables:
-        df_table.rename(columns=trans_dict, inplace=True)
-
-    return tables
--- src/iefp/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-
-from .preview_data_values import preview_all_data
