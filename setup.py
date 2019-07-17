1338592681560036f0bd11f7023405f1274d13a8
Tobias Richter <tr4711@gmail.com>
2019-07-17 10:39:36 +0100

    Pin requirements (#73)

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
-        "PyYAML>=5.1",
-        "luigi>=2.8.7",
-        "pandas>=0.24.2",
-        "psycopg2>=2.8.3",
-        "pyarrow>=0.14.0",
+        "PyYAML==5.1",
+        "luigi==2.8.7",
+        "pandas==0.24.2",
+        "psycopg2==2.8.3",
+        "pyarrow==0.14.0",
         "sqlalchemy==1.2.19",
     ],
 )
