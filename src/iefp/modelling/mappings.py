abdb9737614c9074d00b2b9d58b179c1d38c0887
Tobias Richter <tr4711@gmail.com>
2019-08-08 11:57:17 +0100

    Map nationality to binary (#118)
    * Create boolean mapping for nationality
    
    * Map nationality feature to PT/non-PT

--- src/iefp/modelling/mappings.py
+++ src/iefp/modelling/mappings.py
@@ -1,7 +1,8 @@
 import luigi
-from luigi.contrib.s3 import S3Target
 import pandas as pd
 import yaml
+
+from luigi.contrib.s3 import S3Target
 
 from iefp.modelling import TranslateInterventions
 
@@ -23,43 +24,41 @@ class AddMappings(luigi.Task):
 
     def add_mappings(self, df):
         """
-        Function adds mappings for demographic features like school_qualification
+        Add mappings for demographic features with large number categories
+
         :param df: takes the transformed intermediate df as the input
         :return: returns the df with mappings applied to selected demographic features
         """
-        school_mappings = yaml.load(
-            open("./conf/base/school_mappings.yml"), Loader=yaml.FullLoader
-        )
-        school_mappings = {str(key): value for key, value in school_mappings.items()}
         college_mappings = yaml.load(
             open("./conf/base/college_mappings.yml"), Loader=yaml.FullLoader
         )
         college_mappings = {str(key): value for key, value in college_mappings.items()}
-        cpp_mappings = yaml.load(
-            open("./conf/base/cpp_mappings.yml"), Loader=yaml.FullLoader
-        )
-
-        # Apply school mapping from Nova
-        df["d_school_qualification"] = df["d_school_qualification"].map(school_mappings)
-
-        # Apply college mapping from Nova
+        # NOTE: Take first digit of college qualification for mapping
         df["d_college_qualification"] = (
             df["d_college_qualification"].str[0].map(college_mappings)
         )
 
-        # Apply cpp / job code mappings for previous and desired job features
+        school_mappings = yaml.load(
+            open("./conf/base/school_mappings.yml"), Loader=yaml.FullLoader
+        )
+        school_mappings = {str(key): value for key, value in school_mappings.items()}
+        df["d_school_qualification"] = df["d_school_qualification"].map(school_mappings)
+
+        # NOTE: Take first digit of cpp for mapping
+        cpp_mappings = yaml.load(
+            open("./conf/base/cpp_mappings.yml"), Loader=yaml.FullLoader
+        )
         df["d_desired_job_sector"] = (
             df.d_desired_cpp.dropna().astype(str).str[0].astype(int).map(cpp_mappings)
         )
         df["d_previous_job_sector"] = (
             df.d_previous_cpp.dropna().astype(str).str[0].astype(int).map(cpp_mappings)
         )
-
-        # Drop unnecessary columns
-        df.drop(columns=["d_desired_cpp", "d_previous_cpp"], inplace=True)
+        df = df.drop(columns=["d_desired_cpp", "d_previous_cpp"])
 
         # Apply boolean transformations
         df["d_disabled"] = df["d_disabled"] != 0.0
         df["d_subsidy"] = df["d_subsidy"].notna()
+        df.loc[df["d_nationality"] != "PT", "d_nationality"] = "other"
 
         return df
